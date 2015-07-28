a05acbd03e66697b9adba5f8cc86142f03174cd0
Charles Guan <charlesguan94@gmail.com>
2015-07-27 23:18:12 -0700

    load data in summarizer

--- recon_results/summarize_results.m
+++ recon_results/summarize_results.m
@@ -2,6 +2,8 @@ result_files = dir('~/focuss/recon_results/');
 filenames = { result_files.name};
 
 load('2D_data.mat')
+TRUTH_DATA = func_data; % func_data = data + mask
+TD_NORM = norm(TRUTH_DATA(:));
 
 for idx = 1:length(filenames)
     filename = filenames{idx};
